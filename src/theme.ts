97e758480d8ed4d9c5f87e83b554ffe0eb5c131d
SpearJin <ckdwls2525@gmail.com>
2022-03-12 13:06:39 +0900

    [Feat] 글로벌 스타일 셋팅

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  bgColor: '#28161C',
+  textColor: '#FFFFFF',
+  accentColor: '#ED5E93',
+  cardColor: 'transparent',
+};
