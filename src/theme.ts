30ab6a2555b24633197504d5f6e632d9fda96373
SpearJin <ckdwls2525@gmail.com>
2022-03-12 16:58:45 +0900

    [Fix] 배경색 변경

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
-  bgColor: '#28161C',
+  bgColor: '#2D3436',
   textColor: '#FFFFFF',
   accentColor: '#ED5E93',
   cardColor: 'transparent',
